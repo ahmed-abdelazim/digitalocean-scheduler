362af324e381fd85123a1d1d32fdad913146ff81
Ahmed Abdeazim <34234821+ahmed-abdelazim@users.noreply.github.com>
2023-03-20 23:30:22 +0000

    created snapshot from Droplet and Volume

--- README.md
+++ README.md
@@ -1,3 +1,14 @@
 # digitalocean-scheduler
+
+### Before runnig
+
+Add `TOKEN` as environment variable before deployment (this is the Digital Ocean token)
+
 ### Deploy the function
+
 `doctl serverless deploy .`
+```
+doctl compute droplet-action snapshot 340456218 --snapshot-name '<snapshot-name>'
+doctl compute volume snapshot <volume-id> --snapshot-name '<snapshot-name>'
+
+```
--- packages/scheduler/droplet-snapshot/__main__.py
+++ packages/scheduler/droplet-snapshot/__main__.py
@@ -4,6 +4,12 @@ def main(args):
       manager = digitalocean.Manager(token=os.getenv('TOKEN'))
       my_projects = manager.get_all_projects()
       # name = args.get("name", "stranger")
-      print(my_projects)
-      return {"body": greeting}
+      #print(args.get('droplets'))
+      print(digitalocean.Volume(token=os.getenv('TOKEN'), id="a8258f70-c722-11ed-a838-0a58ac1447fa").snapshot(name="wordpress-backup-vol"))
+      print(digitalocean.Droplet(token=os.getenv('TOKEN'), id="203453589").take_snapshot("Indian-server"))
+      print(digitalocean.Droplet(token=os.getenv('TOKEN'), id="340456218").take_snapshot("Wordpress"))
+      #return {"body":  manager.get_all_droplets()[0].__dict__}
+      return {"body":  "ok"}
       # return {"body": my_projects}
+if __name__ == "__main__":
+      main({'droplets': ['203453589','340456218']})
