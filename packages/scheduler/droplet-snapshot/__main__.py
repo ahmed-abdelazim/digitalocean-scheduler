6e370880a3fd3e0eda01e8264f3d9ae6acf2bb18
Ahmed Abdeazim <34234821+ahmed-abdelazim@users.noreply.github.com>
2023-03-20 23:41:30 +0000

    added snapshot name

--- packages/scheduler/droplet-snapshot/__main__.py
+++ packages/scheduler/droplet-snapshot/__main__.py
@@ -6,8 +6,8 @@ def main(args):
       # name = args.get("name", "stranger")
       #print(args.get('droplets'))
       print(digitalocean.Volume(token=os.getenv('TOKEN'), id="a8258f70-c722-11ed-a838-0a58ac1447fa").snapshot(name="wordpress-backup-vol"))
-      print(digitalocean.Droplet(token=os.getenv('TOKEN'), id="203453589").take_snapshot("Indian-server"))
-      print(digitalocean.Droplet(token=os.getenv('TOKEN'), id="340456218").take_snapshot("Wordpress"))
+      print(digitalocean.Droplet(token=os.getenv('TOKEN'), id="203453589").take_snapshot(snapshot_name="Indian-server"))
+      print(digitalocean.Droplet(token=os.getenv('TOKEN'), id="340456218").take_snapshot(snapshot_name="Wordpress"))
       #return {"body":  manager.get_all_droplets()[0].__dict__}
       return {"body":  "ok"}
       # return {"body": my_projects}
